15be51417e289eca66d1fa9cbc648dcbee3ddfdb
Your Name <you@example.com>
2019-11-27 10:03:06 +0800

    ..

--- src/components/QueryExam.vue
+++ src/components/QueryExam.vue
@@ -8,15 +8,16 @@
       label="序号"
       prop="id"
       align="center"
-      width="180">
+      style="width: 20%;">
     </el-table-column>
 
     <el-table-column
       prop="title"
       align="center"
-      label="考试名称">
+      label="考试名称"
+      style="width: 20%;">
       <template slot-scope="scope">
-      <el-input size="small" v-model="scope.row.title" placeholder="请输入内容" @change="handleEdit(scope.$index, scope.row)" v-if="showEdit[scope.$index]"></el-input> <span v-if="!showEdit[scope.$index]">{{scope.row.name}}</span>
+      <el-input size="small" v-model="scope.row.title" @change="handleEdit(scope.$index)" v-if="scope.row.isSet"></el-input> <span v-if="!scope.row.isSet">{{scope.row.title}}</span>
       </template>
     </el-table-column>
 
@@ -24,23 +25,31 @@
     <el-table-column
       prop="flags"
       align="center"
-      label="标签">
+      label="标签"
+      style="width: 20%;">
+
+      <template slot-scope="scope">
+      <el-input size="small" v-model="scope.row.flags" @change="handleEdit(scope.$index)" v-if="scope.row.isSet"></el-input> <span v-if="!scope.row.isSet">{{scope.row.flags}}</span>
+      </template>
     </el-table-column>
 
     <el-table-column
       prop="date"
       label="日期"
       align="center"
-      width="180">
+      style="width: 20%;">
     </el-table-column>
 
     <el-table-column
-      prop="no"
       label="操作"
       align="center"
-      width="180">
+      style="width: 20%;">
       <template slot-scope="scope">
-    <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+    <el-button-group>
+    <el-button size="small" type="primary" round @click="handleQueryItems(scope.$index, scope.row)">查看</el-button>
+    <el-button size="small" type="warning" round  icon="el-icon-edit"@click="enableEdit(scope.$index, scope.row)">{{scope.row.isSet?'保存':"修改"}}</el-button>
+    <el-button size="small" type="danger" round @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+    </el-button-group>
       </template>
     </el-table-column>
 
@@ -56,70 +65,43 @@
   export default {
     data() {
       return {
-        showEdit: [1,1,0,0], //显示编辑框
         tableData: [{
           id: '1',
           title: '初中英语二年级考试',
           flags: '高三 初一',
           date: '2016-05-02',
-          no: '<a>i'
+          isSet: false
         }, {
           date: '2016-05-04',
           id: '2',
           flags: '高三 初一',
           title: '初中英语二年级考试',
+          isSet: false
         }, {
           date: '2016-05-01',
           id: '3',
           flags: '高三 初一',
           title: '初中英语二年级考试',
+          isSet: false
         },
         {
           date: '2016-05-03',
           id: '4',
-          title: '初中英语二年级考试'
+          title: '初中英语二年级考试',
+          isSet: false
         },
         {
           date: '2016-05-03',
           id: '5',
-          title: '初中英语二年级考试'
+          title: '初中英语二年级考试',
+          isSet: false
         },
         {
           date: '2016-05-03',
           id: '6',
-          title: '初中英语二年级考试'
+          title: '初中英语二年级考试',
+          isSet: false
         },
-        {
-          date: '2016-05-03',
-          id: '7',
-          title: '初中英语二年级考试'
-        },
-        {
-          date: '2016-05-03',
-          id: '8',
-          title: '初中英语二年级考试'
-        },
-        {
-          date: '2016-05-03',
-          id: '9',
-          title: '初中英语二年级考试'
-        },
-       {
-          date: '2016-05-03',
-          id: '10',
-          title: '初中英语二年级考试'
-        },
-        {
-          date: '2016-05-03',
-          id: '11',
-          title: '初中英语二年级考试'
-        },
-        {
-          date: '2016-05-03',
-          id: '12',
-          title: '初中英语二年级考试'
-        },
-
         ]
       }
     },
@@ -127,7 +109,12 @@
           handleDelete(index, row) {
             console.log(index, row.title);
             this.tableData.splice(this.tableData.indexOf(row), 1);
+          },
+          enableEdit(index, row) {
+            console.log(index)
+            row.isSet = !row.isSet;
           }
+
     }
   }
 </script>
