c56192e73947605bc40f1a2bb940ff3424807e6d
Your Name <you@example.com>
2019-11-28 14:46:34 +0800

    okk

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,8 @@
   <div class="hello">
   <router-link to="createexam">创建考试</router-link>
   <router-link to="queryexam">查询考试</router-link>
-  <router-link to="createitem">考试流程</router-link>
+  <router-link to="queryitem">查询考试流程</router-link>
+  <router-link to="createitem">添加考试流程</router-link>
   </div>
 </template>
 
