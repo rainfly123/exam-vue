ed8f3eac6b5c552daa23b7cfb14e047dcde7db7c
Your Name <you@example.com>
2019-12-09 15:35:11 +0800

    adf

--- src/components/QueryExam.vue
+++ src/components/QueryExam.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
+<el-dialog
+  title="提示"
+  :visible.sync="AreyousureDelete"
+  width="30%"
+  center>
+  <span>确认删除</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="AreyousureDelete = false">取 消</el-button>
+    <el-button type="primary" @click="Delete">确 定</el-button>
+  </span>
+</el-dialog>
+<el-dialog
+  title="提示"
+  :visible.sync="AreyousurePublish"
+  width="30%"
+  center>
+  <span>确认操作</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="AreyousurePublish= false">取 消</el-button>
+    <el-button type="primary" @click="Publish">确 定</el-button>
+  </span>
+</el-dialog>
+
+
+
   <h2>考试列表</h2>
  <el-button style="position: fixed;top:50px;left:50px;" icon="el-icon-plus" type="primary" @click="ShowCreateExam=true">创建考试</el-button>
   <createxam v-if="ShowCreateExam" v-on:closeCreatexam="closeCreatexam">
@@ -55,6 +80,8 @@
     <el-button size="small" type="primary" round @click="handleQueryItems(scope.$index, scope.row)">查看</el-button>
     <el-button size="small" type="warning" round  icon="el-icon-edit"@click="enableEdit(scope.$index, scope.row)">{{scope.row.isSet?'保存':"修改"}}</el-button>
     <el-button size="small" type="danger" round @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+    <el-button size="small" type="info" round @click="handlePublish(scope.$index, scope.row)">
+  {{scope.row.published?'已发布':"发布"}}</el-button>
     </el-button-group>
       </template>
     </el-table-column>
@@ -70,18 +97,23 @@
     components: { 'createxam':CreateExam },
     data() {
       return {
+        AreyousureDelete:false,
+        AreyousurePublish:false,
         ShowCreateExam:false,
+        handling:0,
         tableData: [{
-          id: 1,
+          examid: 1,
           title: '初中英语二年级考试',
           flags: '高三 初一',
           date: '2016-05-02',
+          published: true,
           isSet: false
         }, {
           date: '2016-05-04',
           id: 2,
           flags: '高三 初一',
           title: '初中英语二年级考试',
+          published: false,
           isSet: false
         }, {
           date: '2016-05-01',
@@ -140,13 +172,29 @@
           handleDelete(index, row) {
             console.log(index, row.title);
             this.tableData.splice(this.tableData.indexOf(row), 1);
-            this.$message({
-                message: '删除成功',
+            this.AreyousureDelete = true
+            this.handling = row.examid
+          },
+          Delete(){
+              this.AreyousureDelete = false
+              this.$message({
+                message: '删除成功 ' + this.handling,
                 type: 'success',
                 center: 'true',
                 duration: 1000
             });
           },
+          Publish(){
+              this.AreyousurePublish = false
+              this.tableData[this.displayItem].published = !this.tableData[this.displayItem].published 
+              this.$message({
+                message: '操作成功'+this.handling,
+                type: 'success',
+                center: 'true',
+                duration: 1000
+            });
+          },
+
           enableEdit(index, row) {
             if (row.isSet) {
              //save data
@@ -162,6 +210,14 @@
             row.isSet = !row.isSet;
 
           },
+          handlePublish(index, row) {
+             this.handling = row.examid
+             this.displayItem = this.tableData.indexOf(row)
+             //save data
+             console.log(row)
+             this.AreyousurePublish  = true
+          },
+
           handleQueryItems(index, row) {
             this.$router.push({name:'QueryItem',query: {examid: row.id}});
 
